b63021fc18e18bea1f09bfa5c1c847e69e20e48c
fedor <fedvul@MacBook-Pro-fedor.local>
2026-02-13 22:41:44 +0300

    second commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -62,7 +62,7 @@ func UploadHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Ошибка сохранения файла", http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, result)
 }
